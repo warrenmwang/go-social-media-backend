da88c0c31d346754c39ed7ab0268c26d19b25dbe
Warren Wang <warren.wang2020@gmail.com>
2023-01-12 11:38:30 -0800

    updates to db

--- database/database.go
+++ database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"encoding/json"
+	"errors"
 	"os"
 	"time"
 )
@@ -10,6 +12,7 @@ type Client struct {
 	path string
 }
 
+// NewClient -
 // construct a client
 func NewClient(path string) Client {
 	return Client{path}
@@ -37,14 +40,20 @@ type Post struct {
 	Text      string    `json:"text"`
 }
 
-// create new empty db file (json) at path specified by the client
+// create new db file (json) at path specified by the client
+// empty databaseSchema
 // overwrite any previous data in file if existed previously
 func (c Client) createDB() error {
-	err := os.WriteFile(c.path, []byte{}, 0666)
+	db := databaseSchema{
+		Users: make(map[string]User),
+		Posts: make(map[string]Post),
+	}
+	payload, err := json.Marshal(db)
 	if err != nil {
 		return err
 	}
-	return nil
+	err = os.WriteFile(c.path, payload, 0666)
+	return err
 }
 
 // EnsureDB -
@@ -56,5 +65,117 @@ func (c Client) EnsureDB() error {
 		return c.createDB()
 	}
 	// already exists, do nothing
+	return nil
+}
+
+// overwrite db file with the data in given databaseSchema
+// databaseSchema has JSON tags, can marshal to json format byte slice
+func (c Client) updateDB(db databaseSchema) error {
+	payload, err := json.Marshal(db)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(c.path, payload, 0666)
+	return err
+}
+
+// return data read from db at path in client as a databaseSchema
+func (c Client) readDB() (databaseSchema, error) {
+	data, err := os.ReadFile(c.path)
+	if err != nil {
+		return databaseSchema{}, err
+	}
+
+	// convert data from json byte slice to databaseSchema
+	dbFromRead := databaseSchema{}
+	err = json.Unmarshal(data, &dbFromRead)
+	if err != nil {
+		return databaseSchema{}, err
+	}
+
+	return dbFromRead, nil
+}
+
+// CreateUser -
+// email needs to be unique for each user
+func (c Client) CreateUser(email, password, name string, age int) (User, error) {
+	// read current status of db
+	db, err := c.readDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	// create new user
+	newUser := User{
+		CreatedAt: time.Now().UTC(),
+		Email:     email,
+		Password:  password,
+		Name:      name,
+		Age:       age,
+	}
+
+	// add newUser and write to disk
+	db.Users[email] = newUser
+	err = c.updateDB(db)
+	if err != nil {
+		return User{}, err
+	}
+
+	return newUser, nil
+}
+
+// UddateUser -
+// similar to CreateUser but return an error if user doesn't already exist
+// do not update CreatedAt timestamp
+func (c Client) UpdateUser(email, password, name string, age int) (User, error) {
+	// read from db to see if user already exists
+	db, err := c.readDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	// check if email is a key in db.Users
+	if _, ok := db.Users[email]; !ok {
+		return User{}, errors.New("user doesn't exist")
+	}
+	// user does exist, we will update (email and CreatedAt fields won't change)
+	user := db.Users[email]
+
+	user.Password = password
+	user.Name = name
+	user.Age = age
+
+	db.Users[email] = user
+	c.updateDB(db)
+
+	return user, nil
+}
+
+// GetUser -
+// return user given the email from the db
+func (c Client) GetUser(email string) (User, error) {
+	db, err := c.readDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	if user, ok := db.Users[email]; ok {
+		return user, nil
+	} else {
+		return User{}, errors.New("user doesn't exist")
+	}
+}
+
+// DeleteUser -
+// delete a user (via email key) from db
+func (c Client) DeleteUser(email string) error {
+	db, err := c.readDB()
+	if err != nil {
+		return err
+	}
+
+	delete(db.Users, email) // if key doesn't exist, this is no-op
+	c.updateDB(db)          // save changes to disk
+
 	return nil
 }
